1936ae4df57d72db4a200e0441f6b52c67c63fee
lqwangxg@gmail.com <U0Bd9Oa4>
2020-06-17 10:59:35 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,158 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <beautiful-chat 
+      :alwaysScrollToBottom="alwaysScrollToBottom"
+      :close="closeChat"
+      :colors="colors"
+      :isOpen="isChatOpen"
+      :messageList="messageList"
+      :messageStyling="messageStyling"
+      :newMessagesCount="newMessagesCount"
+      :onMessageWasSent="onMessageWasSent"
+      :open="openChat"
+      :participants="participants"
+      :showCloseButton="true"
+      :showLauncher="true"
+      :showEmoji="true"
+      :showFile="true"
+      :showTypingIndicator="showTypingIndicator"
+      :showEdition="true"
+      :showDeletion="true"
+      :titleImageUrl="titleImageUrl"
+      @onType="handleOnType"
+      @edit="editMessage"
+      @remove="removeMessage"
+    >
+      <template v-slot:text-message-toolbox="scopedProps">
+        <button v-if="!scopedProps.me && scopedProps.message.type==='text'" @click.prevent="like(scopedProps.message.id)">
+          👍
+        </button>
+      </template>
+      <template v-slot:text-message-body="scopedProps"> 
+        <p class="sc-message--text-content" v-html="scopedProps.messageText"></p>
+        <p v-if="scopedProps.message.data.meta" class='sc-message--meta' :style="{color: scopedProps.messageColors.color}">{{scopedProps.message.data.meta}}</p>
+        <p v-if="scopedProps.message.isEdited || scopedProps.message.liked" class='sc-message--edited'>
+          <template v-if="scopedProps.message.isEdited">✎</template>
+          <template v-if="scopedProps.message.liked">👍</template>
+        </p>
+      </template>
+      <template v-slot:system-message-body="{ message }">
+        [System]: {{message.text}}
+      </template>
+    </beautiful-chat>
   </div>
 </template>
 
 <script>
+
+import messageHistory from './messageHistory'
+import chatParticipants from './chatProfiles'
+import availableColors from './colors'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      participants: chatParticipants,
+      titleImageUrl:
+        'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
+      messageList: messageHistory,
+      newMessagesCount: 0,
+      isChatOpen: false,
+      showTypingIndicator: '',
+      colors: null,
+      availableColors,
+      chosenColor: null,
+      alwaysScrollToBottom: true,
+      messageStyling: true,
+      userIsTyping: false
+    }
+  },
+  created() {
+    this.setColor('blue')
+  },
+  methods: {
+    sendMessage(text) {
+      if (text.length > 0) {
+        this.newMessagesCount = this.isChatOpen
+          ? this.newMessagesCount
+          : this.newMessagesCount + 1
+        this.onMessageWasSent({
+          author: 'support',
+          type: 'text',
+          id: Math.random(),
+          data: { text }
+        })
+      }
+    },
+    handleTyping(text) {
+      this.showTypingIndicator =
+        text.length > 0
+          ? this.participants[this.participants.length - 1].id
+          : ''
+    },
+    onMessageWasSent(message) {
+      this.messageList = [...this.messageList, Object.assign({}, message, {id: Math.random()})]
+    },
+    openChat() {
+      this.isChatOpen = true
+      this.newMessagesCount = 0
+    },
+    closeChat() {
+      this.isChatOpen = false
+    },
+    setColor(color) {
+      this.colors = this.availableColors[color]
+      this.chosenColor = color
+    },
+    showStylingInfo() {
+      this.$modal.show('dialog', {
+        title: 'Info',
+        text:
+          'You can use *word* to <strong>boldify</strong>, /word/ to <em>emphasize</em>, _word_ to <u>underline</u>, `code` to <code>write = code;</code>, ~this~ to <del>delete</del> and ^sup^ or ¡sub¡ to write <sup>sup</sup> and <sub>sub</sub>'
+      })
+    },
+    messageStylingToggled(e) {
+      this.messageStyling = e.target.checked
+    },
+    handleOnType() {
+      this.$root.$emit('onType')
+      this.userIsTyping = true
+    },
+    editMessage(message){
+      const m = this.messageList.find(m => m.id === message.id);
+      m.isEdited = true;
+      m.data.text = message.data.text;
+    },
+    removeMessage(message){
+      if (confirm('Delete?')){
+        const m = this.messageList.find(m => m.id === message.id);
+        m.type = 'system';
+        m.data.text = 'This message has been removed';
+      }
+    },
+    like(id){
+      const m = this.messageList.findIndex(m => m.id === id);
+      var msg = this.messageList[m];
+      msg.liked = !msg.liked;
+      this.$set(this.messageList, m, msg);
+    }
+  },
+  computed: {
+    linkColor() {
+      return this.chosenColor === 'dark'
+        ? this.colors.sentMessage.text
+        : this.colors.launcher.bg
+    },
+    backgroundColor() {
+      return this.chosenColor === 'dark' ? this.colors.messageList.bg : '#fff'
+    }
+  },
+  mounted(){
+    this.messageList.forEach(x=>x.liked = false);
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Chat from './Chat_plugin.js'
+import vmodal from 'vue-js-modal'
 Vue.config.productionTip = false
+Vue.use(Chat)
+Vue.use(vmodal)
 
 new Vue({
   render: h => h(App),
