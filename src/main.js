b55f4048ea2ecf3edb4762eb77f42870e8103764
lqwangxg@gmail.com <U0Bd9Oa4>
2020-06-19 14:58:06 +0900

    vue-plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 //import Chat from './components/vue-beautiful-chat.umd.min.js'
-import Chat from '../vue_chat_plugin/Chat_plugin.js'
+import Chat from '../vue_chat_plugin/index.js'
 import vmodal from 'vue-js-modal'
 Vue.config.productionTip = false
 Vue.use(Chat)
